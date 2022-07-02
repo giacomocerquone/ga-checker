3f2f328cc4416e8ab1dbc1097682a1a806ef2dce
giacomocerquone <cerquone96@hotmail.it>
2022-07-02 18:24:33 +0200

    add gifs

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -1,7 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 export default async function handler(req, res) {
   try {
+    if (!req.query.url || !isValidHttpUrl(req.query.url)) {
+      throw new Error();
+    }
     const fetchRes = await fetch(req.query.url);
     const html = await fetchRes.text();
 
@@ -9,6 +24,6 @@ export default async function handler(req, res) {
       .status(200)
       .json({ ga: new RegExp(/GoogleAnalyticsObject|ga\(/).test(html) });
   } catch (e) {
-    res.status(500);
+    res.status(500).json({ error: true });
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,27 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
+const SubmitIcon = (props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
+      <path
+        fill="#fff"
+        d="m190.5 66.9 22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"
+      />
+    </svg>
+  );
+};
+
 export default function Home() {
   const [url, setUrl] = useState("");
+  const [usingGa, setUsingGa] = useState();
 
   const onSubmit = (e) => {
     e.preventDefault();
     fetch(`/api/scrape?url=${url}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setUsingGa(data?.ga);
       });
   };
 
@@ -28,18 +40,50 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Check if it's using <br />
+          Check if it&apos;s using <br />
           <a href="https://isgoogleanalyticsillegal.com/">Google Analytics</a>
         </h1>
-
         <form className={styles.form} onSubmit={onSubmit}>
           <input
+            placeholder="https://..."
             type="text"
             onChange={(e) => setUrl(e.target.value)}
             value={url}
           />
-          <button type="submit">go</button>
+          <button type="submit">
+            <SubmitIcon style={{ width: 24, height: 24 }} />
+          </button>
         </form>
+
+        <div className={styles.answer}>
+          {usingGa === true && (
+            <>
+              <h2>YES!</h2>
+              <iframe
+                src="https://giphy.com/embed/W9WSk4tEU1aJW"
+                width="480"
+                height="270"
+                frameBorder="0"
+                className="giphy-embed"
+                allowFullScreen
+              />
+            </>
+          )}
+
+          {usingGa === false && (
+            <>
+              <h2>NO!</h2>
+              <iframe
+                src="https://giphy.com/embed/15aGGXfSlat2dP6ohs"
+                width="480"
+                height="272"
+                frameBorder="0"
+                className="giphy-embed"
+                allowFullScreen
+              />
+            </>
+          )}
+        </div>
       </main>
 
       <footer className={styles.footer}>
